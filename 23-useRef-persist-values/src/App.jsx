2598b9128550b71fc5e2405463f7fe640a45a0bc
Anamd-hub <anasmdl120@gmail.com>
2025-04-16 00:25:39 +0300

    Update app

--- 23-useRef-persist-values/src/App.jsx
+++ 23-useRef-persist-values/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
+  const renderCount = useRef(1);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  renderCount.current++;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="container">
+      <h1>useRef to Persist Values</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <p>
+        This component rendered <strong>{renderCount.current}</strong> times.
       </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
