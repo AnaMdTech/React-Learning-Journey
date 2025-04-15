5ff0aee90cb49838110c6aa07a239cbd8c32407f
Anamd-hub <anasmdl120@gmail.com>
2025-04-15 09:57:14 +0300

    Update app

--- 18-useEffect-conditional-fetching/src/App.jsx
+++ 18-useEffect-conditional-fetching/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!shouldFetch || userId === null) return;
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${userId}`
+        );
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        console.error("Fetch failed", err);
+      } finally {
+        setLoading(false);
+        setShouldFetch(false);
+      }
+    };
+
+    fetchData();
+  }, [shouldFetch, userId]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: "2rem" }}>
+      <button
+        onClick={() => {
+          setData(null)
+          const randomId = Math.floor(Math.random() * 10) + 1;
+          setUserId(randomId);
+          setShouldFetch(true);
+        }}
+      >
+        Fetch Random User
+      </button>
+
+      {loading && <p>Loading...</p>}
+
+      {data && (
+        <div style={{ marginTop: "1rem" }}>
+          <h2>{data.name}</h2>
+          <p>Email: {data.email}</p>
+          <p>City: {data.address.city}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
