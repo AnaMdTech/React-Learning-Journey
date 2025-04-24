b69c4e91bce79b4d5f255ac5291509f3ead4e8e7
Anamd-hub <anasmdl120@gmail.com>
2025-04-24 12:36:38 +0300

    Set up route configuration with nested routing for dashboard

--- 35-nested-routes-layout/src/App.jsx
+++ 35-nested-routes-layout/src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import DashboardHome from "./pages/Dashboard/DashboardHome";
+import Analytics from "./pages/Dashboard/Analytics";
+import Reports from "./pages/Dashboard/Reports";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-600">Hello</h1>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard">
+            <Route index element={<DashboardHome />} />
+            <Route path="analytics" element={<Analytics />} />
+            <Route path="reports" element={<Reports />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
